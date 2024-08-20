bad7b65b028c0753effd8c3ee10b37f9dfe603f9
SpandanChetia <spandanpriyam1976@gmail.com>
2024-08-20 22:05:10 +0530

    Files Added

--- createTable.sql
+++
@@ -1,9 +0,0 @@
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'school')
-BEGIN
-    CREATE DATABASE school;
-END
-
-IF EXISTS (SELECT * FROM sys.databases WHERE name = 'school')
-BEGIN
-    DROP DATABASE school;
-END
---
+++ sl3_dataSecurity.sql
@@ -0,0 +1,25 @@
+-- HOW TO ADD SECURITY TO YOUR DATABASE ? -- 
+
+CREATE USER  'username'@'localhost' IDENTIFIED BY 'password';
+
+-- username of the user and localhost IP address 
+
+-- Grant Permissions 
+
+GRANT SELECT, INSERT ON school.students TO 'username'@'localhost';
+
+-- Grant additional permissions
+
+GRANT UPDATE ON school.students FROM 'username'@'localhost';
+
+-- Revoke Permissions
+
+REVOKE SELECT, INSERT ON school.students FROM 'username'@'localhost';
+
+-- Change Passwords
+
+ALTER USER 'username'@'localhost' IDENTIFIED BY 'encryptedPassword';
+
+-- Delete a User
+
+DROP USER 'username'&'localhost';
